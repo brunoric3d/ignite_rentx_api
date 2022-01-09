56d7bde51ca3744f4576e4f98ba25447bb0685d3
Bruno Ricardo Kruger Pereira <brunoric3d@gmail.com>
2022-01-09 17:36:24 -0300

    fix to db connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,5 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database"): Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions();
-  return createConnection(
-    Object.assign(defaultOptions, {
-      host,
-    })
-  );
+export default async (): Promise<Connection> => {
+  return createConnection();
 };
