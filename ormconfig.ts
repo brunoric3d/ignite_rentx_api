1027adb8a5bf4c0849db6c9f9e7629214885426a
Bruno Ricardo Kruger Pereira <brunoric3d@gmail.com>
2022-01-16 12:03:01 -0300

    Fix ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,14 +4,15 @@ dotenv.config();
 
 export = {
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "docker",
-  password: "ignite",
-  database: "rentx",
+  host: process.env.POSTGRES_DB_URL,
+  port: process.env.POSTGRES_DB_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   migrations: ["./src/database/migrations/*.ts"],
   entities: ["./src/modules/**/entities/*.ts"],
   autoLoadEntities: true,
+  migrationsRun: true,
   cli: {
     migrationsDir: "./src/database/migrations",
   },
