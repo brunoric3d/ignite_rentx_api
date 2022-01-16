07fde841521bbb6bab092a410fa64b4a63904271
Bruno Ricardo Kruger Pereira <brunoric3d@gmail.com>
2022-01-16 13:38:40 -0300

    Fix orm connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,8 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database"): Promise<Connection> => {
+export default async (
+  host = process.env.POSTGRES_DB_URL
+): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
