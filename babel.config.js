32d9b46ad96a991be606aec0d81271a2775ca2b8
Bruno Ricardo Kruger Pereira <brunoric3d@gmail.com>
2022-01-19 00:46:15 -0300

    Fix to compiler settings

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,10 @@ module.exports = {
     "@babel/preset-typescript",
   ],
   plugins: [
-    ["module-resolver"],
+    [
+      "module-resolver"
+      
+    ],
     "babel-plugin-transform-typescript-metadata",
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
