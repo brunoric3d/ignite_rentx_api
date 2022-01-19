6415f62e2632c3e31c837583a020ebf40a0ffccf
Bruno Ricardo Kruger Pereira <brunoric3d@gmail.com>
2022-01-19 00:31:39 -0300

    Build fix

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 module.exports = {
   type: "postgres",
   host: process.env.POSTGRES_DB_URL,
